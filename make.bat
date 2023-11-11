9ded41cb3ec19b2608283a20016d42e3cfa56745
shannon-greenlight <shannon@greenlightdevelopment.com>
2023-11-10 21:36:52 -0800

    Adds error handling and zipping of output.

--- make.bat
+++ make.bat
@@ -1,1 +1,10 @@
-npx electron-forge make
+call npx electron-forge make
+if errorlevel 1 (
+   if [%1]==[] echo Make Error: %errorlevel%
+   set /p exitkey= "Press any key to continue..."
+   exit /b %errorlevel%
+)
+
+del .\out\spank_cp_app.zip
+"C:\Program Files\7-Zip\7z.exe" a .\out\spank_cp_app.zip .\out\make\squirrel.windows\x64\*.exe
+pause
